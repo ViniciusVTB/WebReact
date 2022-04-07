40d71adff13e7138d1e8367d3b7729ef8326b614
Vinicius Teodoro Bitencourt <vinicius-vtb@hotmail.com>
2022-04-07 15:26:50 -0300

    Add project files.

---
+++ WebReact/Configuracao/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebReact.Models;
+
+namespace WebReact.Configuracao
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> option) : base(option)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
